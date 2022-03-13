8ee4814ef52daa6bace67f46ae647d2ac58e7a32
Vasiliy Brave <vskh93.009@gmail.com>
2022-03-13 13:47:59 +0300

    Удаление лишнего кода в NoteListPresenter.

--- app/src/main/java/com/hfad/newnotesmvp/ui/notelist/NoteListPresenter.java
+++ app/src/main/java/com/hfad/newnotesmvp/ui/notelist/NoteListPresenter.java
@@ -1,34 +1,24 @@
 package com.hfad.newnotesmvp.ui.notelist;
 
 import android.content.Context;
-
 import com.hfad.newnotesmvp.data.INotesRepository;
-
 import com.hfad.newnotesmvp.data.NotesRepository;
 import com.hfad.newnotesmvp.data.model.Note;
-
 import java.util.List;
 
 public class NoteListPresenter implements NoteListContract.INoteListPresenter {
 
     NoteListContract.INoteListView view;
-    //    INotesRepository ....
     INotesRepository repository;
 
     public NoteListPresenter(NoteListContract.INoteListView view, Context applicationContext) {
         this.view = view;
-//        loadNotes();
-//        repo = new Repo
         repository = new NotesRepository(applicationContext);
-
     }
 
     public void loadNotes() {
-        //TODO сходить в репозиторий, получить все заметки и передать на вью
         List<Note> notes = repository.getNotes();
         view.showNotes(notes);
-//        repository.getNotes();
-//        view.showNotes();
     }
 
     @Override
